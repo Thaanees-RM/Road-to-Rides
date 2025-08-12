1ae714063e914e9a20699083a002896b758d1742
Fayaz <96859481+fayazamd@users.noreply.github.com>
2025-08-12 15:28:41 +0530

    QA updates

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ import formRoutes from './routes/formRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 
 // Middleware
 app.use(cors());
