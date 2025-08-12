880790057319afc1a38be0752b006c46b620ce6b
Thaanees-RM <thaaneesrafeek11@gmail.com>
2025-08-12 18:55:37 +0530

    Add backend server with root route and DB connection

--- Server/server.js
+++ Server/server.js
@@ -11,16 +11,21 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 // app.use('/uploads', express.static('uploads'));
 
-// Routes
+// Root route to fix "Cannot GET /"
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+// Add your other routes here
 app.use('/api', formRoutes);
 
 // DB Connect
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
-})
-.catch((err) => console.error(err));
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+  })
+  .catch((err) => console.error(err));
